c619e2d4297c6042c482a146805e87ada6ea888d
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-03-27 21:32:07 +0500

    authentication via google account

--- app/src/main/java/com/example/infomedicalstaff/LoginFragment.kt
+++ app/src/main/java/com/example/infomedicalstaff/LoginFragment.kt
@@ -1,18 +1,33 @@
 package com.example.infomedicalstaff
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentTransaction
 import com.example.infomedicalstaff.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private var _binding : FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    lateinit var launcher: ActivityResultLauncher<Intent>
+    lateinit var auth : FirebaseAuth
 
     @SuppressLint("CommitTransaction", "UseRequireInsteadOfGet")
     override fun onCreateView(
@@ -21,6 +36,20 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
+        auth = Firebase.auth
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try {
+                val account = task.getResult(ApiException :: class.java)
+
+                if(account != null){
+                    firebaseAuthWithGoogle(account.idToken!!) //регистрация
+                }
+            } catch (e: ApiException){
+                Log.d("AuthTag", "Google signIn error")
+            }
+        }
+
         binding.tvRegistration.setOnClickListener {
             val registrationFragment = RegistrationFragment()
             val transaction : FragmentTransaction = fragmentManager!!.beginTransaction()
@@ -28,12 +57,41 @@ class LoginFragment : Fragment() {
             transaction.commit()
         }
 
+        //button google
         binding.btnGoogleLogin.setOnClickListener {
-            val homeFragment = HomeFragment()
+            signInWithGoogle()
+
+            /*val homeFragment = HomeFragment()
             val transaction : FragmentTransaction = fragmentManager!!.beginTransaction()
             transaction.replace(R.id.main_layout, homeFragment)
-            transaction.commit()
+            transaction.commit()*/
         }
+
         return binding.root
+    }
+
+    private fun signInWithGoogle(){
+        val singInClient = getClient()
+        launcher.launch(singInClient.signInIntent)
+    }
+
+    //вынести в moxy
+    private fun getClient() : GoogleSignInClient{
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        return GoogleSignIn.getClient(requireActivity(), gso)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken : String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if(it.isSuccessful){
+                Log.d("AuthTag", "Google signIn done")
+            } else Log.d("AuthTag", "Google signIn error")
+        }
     }
 }

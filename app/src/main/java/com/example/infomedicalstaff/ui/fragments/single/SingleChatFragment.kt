0cc21116724bb62d9a593b24fc4884c4eda6183f
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-03-29 20:16:16 +0500

    layout screen single chat

---
+++ app/src/main/java/com/example/infomedicalstaff/ui/fragments/single/SingleChatFragment.kt
@@ -0,0 +1,17 @@
+package com.example.infomedicalstaff.ui.fragments.single
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.infomedicalstaff.R
+import com.example.infomedicalstaff.ui.fragments.BaseFragment
+
+
+class SingleChatFragment :BaseFragment(R.layout.fragment_single_chat){
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
